f87afcd93f0321f1584dab3e6d689fa79c46f84f
Jineesh <john.jineesh@nationwide.co.uk>
2018-03-21 11:43:41 +0000

    JJohn|Add hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
 const path = require('path');
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+ const webpack = require('webpack');
 
-  module.exports = {
-    entry: {
+module.exports = {
+  entry: {
       app: './src/index.js',
-      print: './src/print.js'
-    },
-    devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist',
-     proxy: {
-      '/api/**': {
-        target: 'http://jsonplaceholder.typicode.com/',
-        changeOrigin: true
+      print: './src/print.js',
+      app: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+     hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
-    }
-   },
-    plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
-        title: 'Development'
-      })
-    ],
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/'
-
-    }
-  };
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement'
+    }),
+     new webpack.NamedModulesPlugin(),
+     new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
